740ccd3ec3d6ee590ae04934207c8ae0a495d6fe
Bansal2905 <bnslaman999@gmail.com>
2024-01-05 18:15:37 +0530

    Modified app.py

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ import streamlit as st
 
 model = load_model('model1.h5')
 # nltk.download('stopwords')
-stopwords = set(nltk.corpus.stopwords.words('english'))
+# stopwords = set(nltk.corpus.stopwords.words('english'))
+stopwords = ['had', 'himself', 'needn', "needn't", 've', 'been', 'this', "you're", 'with', 'down', "isn't", 'me', 'who', 'as', 'isn', "weren't", 'i', 'why', 'was', 'are', 'off', 'against', 'during', 'own', "haven't", "that'll", "didn't", "hasn't", 'what', 'to', 'other', 'ours', 'myself', 'about', 'doing', 'same', 'haven', 'which', 'be', 'very', 'now', 'do', "shouldn't", 'll', 'weren', 'than', 'aren', "aren't", 'couldn', 'too', 'we', 'will', 'her', 'his', 'don', "hadn't", 'themselves', 'my', 'just', "mustn't", 'were', 'out', "don't", "won't", 'until', 'yourself', 'both', 'doesn', 'above', 'your', 'you', 'for', 'before', 'between', 'is', 's', 'itself', 'the', 'of', 'having', 'them', 'up', 'here', 'each', 'because', 'hasn', 'by', 'mightn', 'not', 'and', 'he', 'him', 'does', 'in', 'at', 'she', "you'll", 'when', 'some', 'hadn', "wouldn't", 'over', 'few', 'd', 'wouldn', 'there', "should've", 'while', "it's", 're', 'under', 'ain', 'into', 'won', 'm', 'or', 'then', 'yourselves', 'most', 'nor', 'so', 'if', 'further', 'after', 'theirs', "shan't", 'but', 'again', 'herself', 'didn', 'ma', 'ourselves', 'once', 'through', 'o', "doesn't", 'wasn', "mightn't", 'such', 'have', 'below', 'an', 'shouldn', 'these', 'a', 'their', "wasn't", 'did', "she's", 'whom', 'any', 'only', 'should', 'that', 'our', 'am', 't', 'on', 'can', 'yours', 'where', 'those', 'more', 'being', 'all', "you've", 'its', 'mustn', 'hers', 'it', 'they', 'has', 'no', 'shan', "you'd", "couldn't", 'y', 'from', 'how']
 
 model = load_model('model1.h5')
 lb = pickle.load(open('lb.pkl','rb'))
